0da92d75a15a7fadb41c8141b91059da1f13cd6a
jomarmata24 <ramilojomar2001@gmail.com>
2023-10-16 20:00:19 +0800

    first commit

---
+++ app/Http/Controllers/pages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pages extends Controller
+{
+    public function index(){
+        return view('components/header')
+        . view('components/navbar')
+        . view('/dashboard')
+        . view('components/footer');
+    }
+}
